e492aaa909c6b5253a89fbcf6210a3bfb1695351
Agata Porwit <agataporwit@Agatas-MacBook-Pro.local>
2022-07-18 15:23:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# camera_application
+#ap 2022
+Camera app project.
+Use camera to capture an image, create gallery and allow image annotations 
---
+++ android/app/src/main/kotlin/com/agataporwit/camera_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.agataporwit.camera_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
